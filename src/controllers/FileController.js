a9ced4e32c79719b6a5dcb251a4a62811bda5ec8
Gustavo Richard <gustavo.richard@outlook.com.br>
2021-07-24 19:15:13 -0300

    Correção para que fosse feito upload do arquivo sem o uri dele ficar como localhost para o mobile.

--- src/controllers/FileController.js
+++ src/controllers/FileController.js
@@ -3,16 +3,16 @@ const File = require('../models/File');
 
 class FileController {
     async store(req, res) {
-        const box = await Box.findById(req.params.id); //ok
+        const box = await Box.findById(req.params.id);
 
         const file = await File.create({
             title: req.file.originalname,
             path: req.file.key,
-        }); //ok
+        });
 
-        box.files.push(file); //ok
+        box.files.push(file);
 
-        await box.save(); //ok
+        await box.save();
 
         req.io.sockets.in(box._id).emit('file', file);
 
--- src/models/File.js
+++ src/models/File.js
@@ -16,7 +16,7 @@ const File = new mongoose.Schema({
 });
 
 File.virtual('url').get(function () {
-    const url = process.env.URL || 'http://localhost:3333'
+    const url = 'http://192.168.3.13:3333'
     return `${url}/files/${encodeURIComponent(this.path)}`;
 })
 
